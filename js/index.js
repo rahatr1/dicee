2b096de066de33b02cf383ac9724beb8ac04af99
Rahat Polash <100351668+rahatr1@users.noreply.github.com>
2022-08-09 16:26:59 +0600

    update

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.getElementById('dicebtn').onclick = () => {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var diceImage1 = "images/dice" + randomNumber1 + ".png";
-var diceImage2 = "images/dice" + randomNumber2 + ".png";
+  var diceImage1 = "images/dice" + randomNumber1 + ".png";
+  var diceImage2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelector(".img1").setAttribute("src", diceImage1);
-document.querySelector(".img2").setAttribute("src", diceImage2);
+  var img1 = document.getElementById('im1');
+  var img2 = document.getElementById('im2');
 
+  img1.src = diceImage1;
+  img2.src = diceImage2;
 
+  var h1 = document.getElementById('h1');
 
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML =
-    "Player1 Won!!";
-} else if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Tie ,Roll again!";
-} else {
-  document.querySelector("h1").innerHTML =
-    "Player2 Won!!";
+  if(randomNumber1 > randomNumber2){
+    h1.innerHTML = "Player 1 Won";
+  } else if (randomNumber1 == randomNumber2){
+    h1.innerHTML = "Tie, Roll Again";
+  } else {
+    h1.innerHTML = "Player2 Won";
+  }
 }
